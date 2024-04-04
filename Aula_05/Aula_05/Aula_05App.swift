e83257a30e5c9af30dbe94458261594c39104a24
Pedro Cavallazzi <111320117+PedroCavallazzi@users.noreply.github.com>
2024-04-04 11:59:43 -0300

    Add files via upload

---
+++ Aula_05/Aula_05/Aula_05App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_05App.swift
+//  Aula_05
+//
+//  Created by Turma01-9 on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Aula_05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
